13c9f665eb0b29aad224cd224469c3c52a315ce8
Duncan <loxa10077@gmail.com>
2021-06-24 15:41:34 +0800

    Create robot-open-browser.rb

---
+++ robot-open-browser.rb
@@ -0,0 +1,14 @@
+require 'selenium-webdriver'
+# require 'pry'
+
+# binding.pry
+# driver = Selenium::WebDriver.for:chrome
+
+# driver.get 'http://google.com/'
+
+
+v1 = ARGV[0]
+# 可從 command line 傳參數近來
+# $ ruby robot-open-browser.rb 'haha'
+
+puts v1
